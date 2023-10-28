38c4386837ce19ec04592e17a0f18db978e39da5
theowiersema <theowiersema@users.noreply.github.com>
2023-10-28 12:34:25 -0500

    clean up demo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,39 +12,31 @@ import ComponentLabel from "./ComponentLabel";
 import SingleSelect from "../components/SingleSelect";
 
 const Home = () => {
-    // multi-select state
-    const [selectedItems, setSelectedItems] = useState<IIdName[]>([
+    const [multiSelectApiItems, setMultiSelectApiItems] = useState<IIdName[]>([
         fakeData[5],
         fakeData[4],
         fakeData[3],
     ]);
-
-    // color picker state
     const [color, setColor] = useState<IColor>();
-
-    // single select api state
-    const [selectedItemApi, setSelectedItemApi] = useState<IIdName | null>(
-        fakeData[7]
-    );
-
-    // single select state
-    const [selectedItem, setSelectedItem] = useState<IIdName | null>(
+    const [singleSelectApiItem, setSingleSelectApiItem] =
+        useState<IIdName | null>(fakeData[7]);
+    const [singleSelectItem, setSingleSelectItem] = useState<IIdName | null>(
         fakeData[3]
     );
 
     return (
         <div className="px-10 flex flex-col space-y-10 mt-10">
-            <ComponentLabel label="Multi-Select">
+            <ComponentLabel label="Multi-Select API">
                 <MultiSelect
                     onItemRemove={(item) =>
-                        setSelectedItems((prev) => [
+                        setMultiSelectApiItems((prev) => [
                             ...prev.filter((x) => x.id !== item.id),
                         ])
                     }
                     onItemSelect={(item) =>
-                        setSelectedItems((prev) => [...prev, item])
+                        setMultiSelectApiItems((prev) => [...prev, item])
                     }
-                    selectedItems={selectedItems}
+                    selectedItems={multiSelectApiItems}
                     lookupFunction={fakeApiCall}
                     label="Select fruits"
                     debounceInMilliseconds={600}
@@ -53,8 +45,8 @@ const Home = () => {
             </ComponentLabel>
             <ComponentLabel label="Single-Select API">
                 <SingleSelect
-                    onUpdate={(item) => setSelectedItemApi(item)}
-                    selectedItem={selectedItemApi}
+                    onUpdate={(item) => setSingleSelectApiItem(item)}
+                    selectedItem={singleSelectApiItem}
                     lookupFunction={fakeApiCall}
                     label="Select a fruit from our database"
                     debounceInMilliseconds={600}
@@ -63,8 +55,8 @@ const Home = () => {
             </ComponentLabel>
             <ComponentLabel label="Single-Select">
                 <SingleSelect
-                    onUpdate={(item) => setSelectedItem(item)}
-                    selectedItem={selectedItem}
+                    onUpdate={(item) => setSingleSelectItem(item)}
+                    selectedItem={singleSelectItem}
                     items={fakeData}
                     label="Select a fruit"
                     debounceInMilliseconds={600}
